e881b4231672f21496a8a2704aa5f800bbcaada8
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-06-19 11:27:50 -0400

    finished scope explanation

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,10 @@ myFunction();
 
 // Explanation: 
 
+// The variable internal is nested inside of the myFunctional. This means it cannot be accessed outside of the function, but can be used by anything else withing the contents of the myFunctional. Since nestedFunction is inside myFunctional, it can reach outside into the larger functional scope, and use this variable without any problems. In general, things are able to reach out into the wider levels of scope, but not down into the narrower ones.
 
 /* Task 2: Counter */
+
+
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
